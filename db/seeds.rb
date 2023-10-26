3a53f320e8b6cba2c0a9984f736ddc74da34029e
R4ja34 <raja.lalaina@gmail.com>
2023-10-27 01:51:59 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+city_names = ["Paris", "Marseille", "Toulouse", "Montpellier"]
+
+city_names.each do |name|
+  City.create(name: name)
+end
+
+25.times do
+  dog = Dog.create(
+    name: Faker::Creature::Dog.breed,
+    city_id: City.all.sample.id
+  )
+end
+
+10.times do
+  dogsitter = Dogsitter.create(
+    name: Faker::Name.first_name,
+    city_id: City.all.sample.id
+  )
+end
+
+dogsitters = Dogsitter.all
+dog = Dog.all
+
+30.times do
+  dogsitter = Dogsitter.all.sample  # Sélectionnez un dogsitter aléatoire parmi tous les existants
+  city = City.find(dogsitter.city_id)
+  dogs_in_same_city = Dog.where(city_id: city.id)
+  date = Faker::Time.between_dates(from: Date.today, to: 1.year.from_now, period: :all)
+  num_dogs = rand(3..6)
+  dogs_to_walk = dogs_in_same_city.sample(num_dogs)
+  dogs_to_walk.each do |dog|
+    Stroll.create(date: date, dogsitter: dogsitter, dog: dog, city: city)
+  end
+end
